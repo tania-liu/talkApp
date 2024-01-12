4bb1b8ee7c66d6680f46d45c47ba013b6ed73f42
Tania Liu <tanialiu430006@gmail.com>
2024-01-12 11:21:26 +0800

    Initial Commit

---
+++ talkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  talkApp
+//
+//  Created by 劉俐廷 on 2024/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
